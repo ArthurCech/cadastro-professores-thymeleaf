3d63dbd7a60847c7ae93e72f27c99e765815ebfe
Arthur Cech Francisco <arthurcech@gmail.com>
2022-08-30 17:39:51 -0300

    feat: add repository for Professor

---
+++ src/main/java/com/arthurcech/regescweb/repositories/ProfessorRepository.java
@@ -0,0 +1,9 @@
+package com.arthurcech.regescweb.repositories;
+
+import com.arthurcech.regescweb.models.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+}
