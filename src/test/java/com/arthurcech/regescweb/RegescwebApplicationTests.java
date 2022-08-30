52b35b700d3cb580d18bfd199fbef4b826a66463
Arthur Cech Francisco <arthurcech@gmail.com>
2022-08-30 14:40:10 -0300

    initial commit

---
+++ src/test/java/com/arthurcech/regescweb/RegescwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthurcech.regescweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegescwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
