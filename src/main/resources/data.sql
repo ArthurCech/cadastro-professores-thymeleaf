055a2c86e65c1a7eea8712a32632bcf4c2dd6740
Arthur Cech Francisco <arthurcech@gmail.com>
2022-09-05 15:20:37 -0300

    database: initialize database with four professors

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO PROFESSOR (NOME, SALARIO, STATUS_PROFESSOR)
+VALUES ('Batman', 15000.00, 'ATIVO'),
+       ('Coringa', 25000.00, 'INATIVO'),
+       ('Robin', 18000.00, 'APOSENTADO'),
+       ('Mulher Maravilha', 14000.00, 'AFASTADO');
