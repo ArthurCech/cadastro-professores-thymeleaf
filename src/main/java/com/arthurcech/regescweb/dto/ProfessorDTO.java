c1e2f79198b62763ea39cf9061da3eaa797da709
Arthur Cech Francisco <arthurcech@gmail.com>
2023-02-20 16:26:32 -0300

    refactor: renamed ProfessorDTO

--- src/main/java/com/arthurcech/regescweb/dto/ProfessorDTO.java
+++ src/main/java/com/arthurcech/regescweb/dto/ProfessorDTO.java
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
-public class RequisicaoFormProfessor {
+public class ProfessorDTO {
 
     @NotBlank
     private String nome;
